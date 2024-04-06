da420eec6468f7435c5383071afcbbd0de02c43d
hoetyngchang <max.chang@calibrantinsurance.com>
2024-04-06 18:00:50 +0100

    first commit

---
+++ app/spotify-to-apple-music/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Head from "next/head";
+import { useEffect } from "react";
+
+import { authentication } from "../lib/apple-music-api-wrapper/authentication";
+
+export default function SpotifyToAppleMusicPage() {
+  useEffect(() => {
+    // console.log("RUN");
+    // async function fetchData() {
+    //   const musicKitInstance = await authentication();
+    //   // musicKitInstance.authorize();
+    // }
+    // fetchData();
+  }, []);
+
+  return (
+    <div>
+      <script
+        src="https://js-cdn.music.apple.com/musickit/v3/musickit.js"
+        data-web-components
+        async
+      ></script>
+    </div>
+  );
+}
