f6f562dd2f243647e141058411311be68e81dc49
hoetyngchang <max.chang@calibrantinsurance.com>
2024-05-19 17:16:03 +0100

    improved destination page

--- app/migrate/_ui/DestinationSelection.tsx
+++ app/migrate/_ui/DestinationSelection.tsx
@@ -1,43 +1,65 @@
 import Image, { StaticImageData } from "next/image";
 import RoundedBorderCard from "@/app/_ui/card/RoundedBorderCard";
-import RountedButton from "@/app/_ui/buttons/RoundedButton";
-import Link from "next/link";
+import clsx from "clsx";
 
 interface DestinationSelectionProps {
   logo?: StaticImageData;
-  onClickLink: string;
+  className: string;
   title: string;
   titleHighlight: string;
 }
 
 export default async function DestinationSelection({
   logo,
-  onClickLink,
+  className,
   title,
-  titleHighlight
+  titleHighlight,
 }: DestinationSelectionProps) {
   return (
-    <RoundedBorderCard className="flex flex-nowrap flex-col justify-between items-stretch"> {/* < Tailwind here doesnt work */}
-    
+    <RoundedBorderCard
+      className={clsx(
+        "flex flex-nowrap flex-col justify-between items-stretch",
+        className
+      )}
+    >
+      {" "}
+      {/* < Tailwind here doesnt work */}
       {logo && (
         <Image
           src={logo}
           alt="Github Logo"
           width={500}
           height={500}
-          className="w-full h-auto cursor-pointer"
+          className="w-full h-auto"
           priority
         />
       )}
-      {title && <h2 className="block w-full self-center self-end font-bold text-xl mt-auto leading-6">
-          {title.split('\\n').map((line, index) => ( //To insert line break from string
-            <span className="opacity-50" key={index}>{line}<br /></span>
-          ))}
-          {titleHighlight.split('\\n').map((line, index) => ( //To insert line break from string
-            <span key={index}>{line}<br /></span>
-          ))}
-      </h2>}
-      
+      {title && (
+        <h2 className="block w-full self-end font-bold text-xl mt-auto leading-6">
+          {title.split("\\n").map(
+            (
+              line,
+              index //To insert line break from string
+            ) => (
+              <span className="opacity-50" key={index}>
+                {line}
+                <br />
+              </span>
+            )
+          )}
+          {titleHighlight.split("\\n").map(
+            (
+              line,
+              index //To insert line break from string
+            ) => (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            )
+          )}
+        </h2>
+      )}
     </RoundedBorderCard>
   );
 }
--- app/migrate/page.tsx
+++ app/migrate/page.tsx
@@ -1,22 +1,24 @@
 import DestinationSelection from "@/app/migrate/_ui/DestinationSelection";
 import SpotifyToAppleImage from "@/public//Spotify-Apple.png";
 import AppleToSpotifyImage from "@/public//Apple-Spotify.png";
-import RoundedBorderCard from "@/app/_ui/card/RoundedBorderCard";
+import Link from "next/link";
 
 export default async function Migrate() {
   return (
     <div className="flex flex-col justify-center items-center h-full transition-all ease-in-out animate-fadeInUpFast ">
       <h1 className="text-3xl text-center font-bold p-6">Migrate Playlists</h1>
       <div className="grid grid-cols-1 pb-10 sm:grid-cols-2 md:grid-cols-2 gap-12 mt-6">
-        <DestinationSelection
-          logo={AppleToSpotifyImage}
-          onClickLink="/migrate-to-apple-music"
-          title="Apple Music \n to"
-          titleHighlight="Spotify"
-        />
+        <Link href="/migrate-to-apple-music">
+          <DestinationSelection
+            logo={AppleToSpotifyImage}
+            className="cursor-pointer"
+            title="Apple Music \n to"
+            titleHighlight="Spotify"
+          />
+        </Link>
         <DestinationSelection
           logo={SpotifyToAppleImage}
-          onClickLink="/migrate-to-apple-music"
+          className="cursor-not-allowed bg-white/15 transition-none hover:bg-white/15 "
           title="Spotify \n to"
           titleHighlight="Apple Music"
         />
