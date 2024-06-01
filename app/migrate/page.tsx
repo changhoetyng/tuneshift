a4ca92315d3c351b9f47b8b13dc4a69238d9bc2d
weizenyang <edmund@edify.ac>
2024-06-01 18:19:32 +0100

    Loading Component

--- app/migrate/page.tsx
+++ app/migrate/page.tsx
@@ -2,6 +2,7 @@
 import DestinationSelection from "@/app/migrate/_ui/DestinationSelection";
 import SpotifyToAppleImage from "@/public//Spotify-Apple.png";
 import AppleToSpotifyImage from "@/public//Apple-Spotify.png";
+import LoadingComponent from "../_ui/global/LoadingComponent";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -9,16 +10,23 @@ import { useRouter } from "next/navigation";
 export default function Migrate() {
 
   const [isAnimating, setIsAnimating] = useState(false);
+  const [countDown, setCountDown] = useState(0);
   const router = useRouter();
 
   const handleClick = (e: React.MouseEvent, destination: string) => {
     e.preventDefault();
     setTimeout(() => {
       router.push(destination);
-    }, 250); 
+    }, 250);
+
+    setInterval(() => {
+      setCountDown(countDown + 33.3)
+    }, 33.3)
   };
 
-  return (
+
+
+  return (countDown < 600 ?
     <div className={`flex flex-col justify-center items-center h-full transition-all ease-in-out animate-fadeInUpFast ${isAnimating ? "animate-fadeOutUpFast" : ""}`}>
       <h1 className="text-3xl text-center font-bold p-6">Migrate Playlists</h1>
       <div className="grid grid-cols-1 pb-10 sm:grid-cols-2 md:grid-cols-2 gap-12 mt-6">
@@ -49,5 +57,10 @@ export default function Migrate() {
         </RoundedBorderCard> */}
       </div>
     </div>
+    :
+    <div className="flex justify-center h-full">
+      <LoadingComponent />
+    </div>
+    
   );
 }
